b5ece533ed23d1d74bf86db480d3c1308e89913b
amkamir82 <amk_amir82@yahoo.com>
2024-11-08 16:53:52 +0330

    add test register user with email

--- UserManagement/src/test/java/ir/selab/tdd/UserServiceTest.java
+++ UserManagement/src/test/java/ir/selab/tdd/UserServiceTest.java
@@ -19,6 +19,9 @@ public class UserServiceTest {
         userService = new UserService(userRepository);
         userService.registerUser("admin", "1234");
         userService.registerUser("ali", "qwert");
+        userService.registerUser("amirmahdi", "kousheshi", "amk_amir82@yahoo.com");
+        userService.registerUser("amirhossein", "arabzadeh", "amirarab888@yahoo.com");
+        userService.registerUser("ahmadreza", "khenari", "arezekhanari@gmail.com");
     }
 
     @Test
@@ -34,6 +37,24 @@ public class UserServiceTest {
         String username = "ali";
         String password = "123abc";
         boolean b = userService.registerUser(username, password);
+        assertFalse(b);
+    }
+
+    @Test
+    public void createNewUserWithEmail_ShouldSuccess() {
+        String username = "test_a_usrnm";
+        String password = "test_a_psw";
+        String email = "test@test.com";
+        boolean b = userService.registerUser(username, password, email);
+        assertTrue(b);
+    }
+
+    @Test
+    public void createNewDuplicateUserWithEmail__ShouldFail() {
+        String username = "test_a";
+        String password = "test_a";
+        String email = "amk_amir82@yahoo.com";
+        boolean b = userService.registerUser(username, password, email);
         assertFalse(b);
     }
 
